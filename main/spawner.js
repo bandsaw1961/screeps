f6e7a117a5baad460f3fb5fddb6b4ca84c73f2c0
Rob Harvey <rob@ukservers.net>
2016-12-04 16:50:21 +0000

    Refactor min creeps

--- main/spawner.js
+++ main/spawner.js
@@ -8,22 +8,23 @@
  */
 
 module.exports = {
-    run: function() {
-        for (let name in Memory.creeps) {
-            if (Game.creeps[name] === undefined) {
-                delete Memory.creeps[name];
-            }
+
+  creeps: { harvester: 10, upgrader: 4, builder: 4 },
+
+  run: function() {
+    for (let name in Memory.creeps) {
+        if (Game.creeps[name] === undefined) {
+            delete Memory.creeps[name];
         }
-
-        var minimumCreeps = { harvester: 10, upgrader: 4, builder: 1 };
-        var creepCount = { };
-        _.each(Game.creeps, (c) => creepCount[c.memory.role] = (creepCount[c.memory.role] || 0) + 1);
-        _.each(['builder', 'harvester', 'upgrader'], (type) => {
-            console.log(`${type}: ${creepCount[type]}`);
-            if ((creepCount[type] || 0) < minimumCreeps[type]) {
-                Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined, { role: type, working: false});
-            }
-        });
-
     }
+
+    let creepCount = {};
+    _.each(Game.creeps, (c) => creepCount[c.memory.role] = (creepCount[c.memory.role] || 0) + 1);
+    _.each(Object.keys(this.creeps), (type) => {
+      console.log(`${type}: ${creepCount[type]}`);
+      if ((creepCount[type] || 0) < this.creeps[type]) {
+        Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined, { role: type, working: false});
+      }
+    });
+  }
 };
