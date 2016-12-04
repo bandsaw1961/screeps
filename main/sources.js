1313b77c3c305d80c624f878ba48f13a4322b50f
Rob Harvey <rob@ukservers.net>
2016-12-04 18:28:07 +0000

    Only use sources with spare capacity

--- main/sources.js
+++ main/sources.js
@@ -1,7 +1,14 @@
 module.exports = {
 
   findBest: function(creep) {
-    return creep.pos.findClosestByRange(FIND_SOURCES);
+    return creep.pos.findClosestByRange(FIND_SOURCES, {
+      filter: function(object){
+        if(object.energy < object.energyCapacity / 3) {
+          return false;
+        }
+        return true;
+      }
+    });
   },
 
 };
